d1e788d51cbab96d7e9a9727fe7cbbcbe3de7ddb
BraininaBowl <fransvb@gmail.com>
2026-01-22 00:39:26 +0100

    Security fixes

--- app/pages/[id]-[title]/edit/index.vue
+++ app/pages/[id]-[title]/edit/index.vue
@@ -1,8 +1,12 @@
 <template>
-  <main class="index">
+  <main class="index" v-if="loggedIn">
     <h1>Edit recipe</h1>
     <div class="loading" v-if="!recipe">Loading...</div>
     <FormRecipeComponent :recipe="recipe" v-else />
+  </main>
+  <main v-else>
+    <h1>You shouldn't be here.</h1>
+    <p><NuxtLink to="/">Return home</NuxtLink></p>
   </main>
 </template>
 
@@ -10,6 +14,8 @@
 const route = useRoute();
 const recipeId = route.params.id;
 const { recipe, fetchRecipe, fetchRecipes } = useRecipes();
+const { loggedIn } = useUserSession();
+
 onMounted(() => {
   fetchRecipe(recipeId);
 });
--- app/pages/trash/index.vue
+++ app/pages/trash/index.vue
@@ -1,10 +1,16 @@
 <template>
-  <main>
+  <main v-if="loggedIn">
     <h1>Trashed recipes</h1>
     <OverviewRecipesComponent pageType="trash" />
   </main>
+  <main v-else>
+    <h1>You shouldn't be here.</h1>
+    <p><NuxtLink to="/">Return home</NuxtLink></p>
+  </main>
 </template>
 
-<script setup></script>
+<script setup>
+const { loggedIn } = useUserSession();
+</script>
 
 <style lang="css" scoped></style>
